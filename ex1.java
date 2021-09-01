15dfe825e29cf4c2bb80762d49ab117352c8f3a5
Saimom000 <72466102+Saimom000@users.noreply.github.com>
2021-09-01 17:50:15 -0300

    soma

---
+++ ex1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class ex1 {
+    //Usei parte do ex LAB01Q01Aquecimento feito pelo professor, pois não sei mecher muito bem com java
+     public static Scanner sc = new Scanner(System.in);
+  
+     public static int somar(int[] array){
+        int soma = 0;
+         
+        for(int i = 0; i < array.length; i++){
+            soma += array[i];          
+        }
+        return soma;
+     }
+     
+     public static void main (String[] args){
+        System.out.println("    Soma de numero inteiros\nDigite quantos numeros vao ser somados:");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n;i++){
+            System.out.println("Digite o numero "+(i+1));
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Soma:"+somar(array));
+     }
+  }
